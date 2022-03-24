ac0b8c41c07a86bf9cbfbe849c320751f5035e6c
Light-Bak <1bakarysanou@gmail.com>
2022-03-24 15:00:30 +0000

    local2

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,13 @@
-Configuration API
 module.exports = {
-    HOST : "localhost",
+    HOST: "localhost",
     USER: "postgres",
     PASSWORD: "ionic",
     DB: "BleDOR",
     dialect: "postgres",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-  };
+};
